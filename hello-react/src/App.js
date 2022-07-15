f464caac2c565f0a2e2d787fdfca20bf8218eae1
swiftie1230 <swiftie1230@naver.com>
2022-07-15 16:13:18 +0900

    secondDay update

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import { Fragment } from 'react';
+/*
 function App() {
   return (
     <div className="App">
@@ -20,6 +21,31 @@ function App() {
       </header>
     </div>
   );
+}
+*/
+
+function App(){
+  const name = 'REACT';
+  /* 
+  // style을 외부에서 선언 후 사용하는 경우!
+  const style = {
+    // background-color는 backgroundColor와 같이 -가 사라지고 카멜 표기법으로 작성한다.
+    backgroundColor: 'white',
+    color : 'aqua',
+    fontSize : '48px',
+    fontWeight : 'bold',
+    padding : 16 // 단위를 생략하면 px로 지정된다.
+  };
+  */
+  return (
+    <Fragment>
+      {name === 'REACT' ? (
+        <div className = "react">It is {name}!</div>
+      ):(
+        <div className = "react">It is not {name}!</div>
+      )}
+    </Fragment>
+  )
 }
 
 export default App;
